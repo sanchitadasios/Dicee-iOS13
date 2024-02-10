c3f18cb98e6dcedc73a6b45ea5bd37aa3d60f471
SanchitaDas <sanch.frnd@gmail.com>
2024-02-10 15:49:53 +0000

    committing for initial push to github

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlet allows to reference UI elements
+    @IBOutlet weak var diceImageViewFirst: UIImageView!
+    @IBOutlet weak var diceImageViewSecond: UIImageView!
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 5
+    let arrayOfImages = [UIImage(imageLiteralResourceName: "DiceOne.jpg"),UIImage(imageLiteralResourceName: "DiceTwo.jpg"), UIImage(imageLiteralResourceName: "DiceThree.jpg"),UIImage(imageLiteralResourceName: "DiceFour.jpg"),UIImage(imageLiteralResourceName: "DiceFive.jpg"),UIImage(imageLiteralResourceName: "DiceSix.jpg")]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        diceImageViewFirst.image = UIImage(imageLiteralResourceName: "DiceSix.jpg")
+//        diceImageViewSecond.image = UIImage(imageLiteralResourceName: "DiceTwo.jpg")
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+//        print("button got tapped")
+//        diceImageViewFirst.image = UIImage(imageLiteralResourceName: "DiceFour.jpg")
+//        diceImageViewSecond.image = UIImage(imageLiteralResourceName: "DiceThree.jpg")
+//        diceImageViewFirst.image = arrayOfImages[leftDiceNumber]
+        diceImageViewFirst.image = arrayOfImages[Int.random(in: 0...5)]
+        diceImageViewSecond.image=arrayOfImages[Int.random(in: 0...5)]
+//        leftDiceNumber += 1
+//        rightDiceNumber -= 1
+//        print(Int.random(in: 1...10))
+        
+    }
 }
 
